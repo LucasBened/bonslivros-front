7cf87b35d01d439eae26583612d77731fc003998
lucasbened <lussivbsilva1@gmail.com>
2025-05-18 23:36:05 -0300

    criando tudo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { LoginComponent } from '../app/components/pages/login/login.components';
 import { RegisterComponent } from '../app/components/pages/register/register.component';
 import { BookListComponent } from '../app/components/pages/listarLivros/listarLivros.component';
@@ -9,11 +8,11 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'books', component: BookListComponent }
+  { path: 'books', component: BookListComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
